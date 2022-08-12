f0fc8f475ee4fef27b7a7e783481fb8b110367f4
wsl <wslzzy@qq.com>
2022-08-12 17:12:16 +0800

    3-3

---
+++ src/test/java/com/wsl/mybatis/MyBatisTestor.java
@@ -0,0 +1,43 @@
+package com.wsl.mybatis;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+import java.util.*;
+
+//JUNIT单元测试类
+public class MyBatisTestor {
+    /**
+     * 初始化SqlSessionFactory
+     * @throws IOException
+     */
+    @Test
+    public void testSqlSessionFactory() throws IOException {
+        //利用Reader加载classpath下的mybatis-config.xml核心配置文件
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        //初始化SqlSessionFactory对象,同时解析mybatis-config.xml文件
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        System.out.println("SessionFactory load success");
+        SqlSession sqlSession = null;
+        //创建SqlSession对象,SqlSession是JDBC的扩展类,用于与数据库交互
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (sqlSession != null){
+                sqlSession.close();
+            }
+        }
+
+    }
+}
